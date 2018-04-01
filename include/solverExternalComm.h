802dd414391303be145684e411d419abe09405c9
Adam Jirasek <Adam.Jirasek@gmail.com>
2018-03-31 20:19:29 -0600

    fix .h file and call to get_interface_info

--- include/solverExternalComm.h
+++ include/solverExternalComm.h
@@ -3,21 +3,8 @@
 
 #include <Loci.h>
 
-
 namespace flowPsi {
-  void get_interface_info(fact_db &facts);
-
-  void find_mind_noslip(fact_db &facts) ;
-  void find_mind_surf(fact_db &facts) ;
-  void find_mind_surf_node(fact_db &facts) ;
-
-  void create_cell_stencil(fact_db &facts) ;
-
-  bool check_boundary_conditions(fact_db &facts) ;
-  bool check_scalar_units(const options_list &o, std::string option,
-                          std::string unit) ;
-  bool check_vector_units(const options_list &ol,std::string vname,
-                          std::string units) ;
+  void get_interface_info(fact_db );
 }
 
 #endif
